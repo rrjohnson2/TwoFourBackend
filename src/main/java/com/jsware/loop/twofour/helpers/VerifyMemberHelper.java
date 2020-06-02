a3543fb035859fb966311874fa55578aa5f00a4e
Roice Johnson <rjohn230uncc.edu>
2020-06-01 22:35:49 -0400

    points to locations

--- src/main/java/com/jsware/loop/twofour/helpers/VerifyMemberHelper.java
+++ src/main/java/com/jsware/loop/twofour/helpers/VerifyMemberHelper.java
@@ -17,77 +17,69 @@ import com.jsware.loop.twofour.sending.Text;
 
 @Component
 public class VerifyMemberHelper {
-	
+
 	public List<Member> unverified = new ArrayList<Member>();
-	
-	public HashMap<Integer,Integer> code_to_index = new HashMap<Integer, Integer>();
-	
-	public HashMap<Integer,Integer> index_to_code = new HashMap<Integer, Integer>();
-	
+
+	public HashMap<Integer, Integer> code_to_index = new HashMap<Integer, Integer>();
+
+	public HashMap<Integer, Integer> index_to_code = new HashMap<Integer, Integer>();
+
 	private final String from_email = "looooop.inc@gmail.com";
-	private final String from_password ="johnsr41";
-	private final String url ="http://localhost:4200/";
-	private final String annouce="Ccheckout the new winner " + url;
-	private final String[] admins= new String[] {"3366181285"};
-	
+	private final String from_password = "johnsr41";
+	private final String url = "http://twofourpicture-env.eba-avytpez6.us-east-2.elasticbeanstalk.com/";
+	private final String annouce = "Checkout the new winner " + url;
+	private final String[] admins = new String[] { "3366181285" };
+
 	@Autowired
 	EmailandPhoneMessage messageHandler;
-	
-	
-	public void addMember(Member mem)
-	{
-		
-			Random rand = new Random();
-			
-			int code = rand.nextInt(999999);
-			
-			
-			try {
-				
-				sendCode(mem, code);
-				
-				if(unverified.contains(mem))
-				{
-					int old_index = unverified.indexOf(mem);
-					int old_code = index_to_code.get(old_index);
-					
-					
-					code_to_index.remove(old_code);
-					index_to_code.remove(old_index);
-					unverified.remove(old_index);
-					
-				}
-				
-				
-				
-				unverified.add(mem);
-				
-				int index =  unverified.size()-1;
-				code_to_index.put(code,index);
-				index_to_code.put(index, code);
-				
-				countDownToRemoval(code,mem);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
+
+	public void addMember(Member mem) {
+
+		Random rand = new Random();
+
+		int code = rand.nextInt(999999);
+
+		try {
+
+			sendCode(mem, code);
+
+			if (unverified.contains(mem)) {
+				int old_index = unverified.indexOf(mem);
+				int old_code = index_to_code.get(old_index);
+
+				code_to_index.remove(old_code);
+				index_to_code.remove(old_index);
+				unverified.remove(old_index);
+
 			}
-			
-			
+
+			unverified.add(mem);
+
+			int index = unverified.size() - 1;
+			code_to_index.put(code, index);
+			index_to_code.put(index, code);
+
+			countDownToRemoval(code, mem);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	private void sendCode(Member mem, int code) throws IllegalAccessException, ClientProtocolException, IOException {
-		
-		String content = "this is your Two Four Viral Code "+code;
+
+		String content = "this is your Two Four Viral Code " + code;
 		switch (mem.getMessageMedium()) {
 		case PHONE:
-					messageHandler.sendText(new Text(content, mem.getPhone()));
+			messageHandler.sendText(new Text(content, mem.getPhone()));
 			break;
 		case EMAIL:
 			messageHandler.sendEmail(new Email(from_email, mem.getEmail(), "TWO FOUR CODE", content, from_password));
 			break;
 		case BOTH:
 			messageHandler.sendEmail(new Email(from_email, mem.getEmail(), "TWO FOUR CODE", content, from_password));
-			messageHandler.sendText(new Text("this is your Two Four Viral Code "+code, mem.getPhone()));
+			messageHandler.sendText(new Text("this is your Two Four Viral Code " + code, mem.getPhone()));
 			break;
 
 		default:
@@ -98,62 +90,57 @@ public class VerifyMemberHelper {
 	private void countDownToRemoval(int code, Member mem) {
 
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				try {
-					Thread.sleep( 3*60*1000);
+					Thread.sleep(3 * 60 * 1000);
 					removeMember(code, mem);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		}).start();
-		
+
 	}
 
-	public boolean removeMember(int code, Member mem)
-	{
+	public boolean removeMember(int code, Member mem) {
 		try {
-			
-			if(code_to_index.get(code) !=null)
-			{
+
+			if (code_to_index.get(code) != null) {
 				int index = code_to_index.get(code);
-				if(mem.equals(unverified.get(index)))
-				{
+				if (mem.equals(unverified.get(index))) {
 					unverified.remove(index);
 					code_to_index.remove(code);
 					index_to_code.remove(index);
 					return true;
 				}
 			}
-			
-			
-			
+
 			return false;
-			
+
 		} catch (Exception e) {
 			return false;
 		}
 	}
 
 	public void announceText(Member member) throws IllegalAccessException, ClientProtocolException, IOException {
-		
-		messageHandler.sendText(new Text(annouce,member.getPhone()));
-		
+
+		messageHandler.sendText(new Text(annouce, member.getPhone()));
+
 	}
 
 	public void announceEmail(Member member) throws IllegalAccessException, ClientProtocolException, IOException {
-		messageHandler.sendEmail(new Email(from_email, member.getEmail(), "TWO FOUR WINNER",annouce, from_password));
-		
+		messageHandler.sendEmail(new Email(from_email, member.getEmail(), "TWO FOUR WINNER", annouce, from_password));
+
 	}
-	public void alertAdmin() throws IllegalAccessException, ClientProtocolException, IOException
-	{
+
+	public void alertAdmin() throws IllegalAccessException, ClientProtocolException, IOException {
 		for (String string : admins) {
-			 messageHandler.sendText(new Text("Choose a Winner", string));
+			messageHandler.sendText(new Text("Choose a Winner", string));
 		}
-		
+
 	}
 
 }
--- src/main/java/com/jsware/loop/twofour/sending/EmailandPhoneMessage.java
+++ src/main/java/com/jsware/loop/twofour/sending/EmailandPhoneMessage.java
@@ -1,59 +1,52 @@
 package com.jsware.loop.twofour.sending;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Component;
-import org.apache.http.entity.StringEntity;
-
-
 
 @Component
 public class EmailandPhoneMessage {
-	
-	
-	private final String sms_server = "http://localhost:8081/";
-	
-	
-	public void sendEmail(Email em) throws IllegalAccessException, ClientProtocolException, IOException 
-	{
+
+	private final String sms_server = "http://smsemail-env.eba-tbvf2xp5.us-east-2.elasticbeanstalk.com/";
+
+	public void sendEmail(Email em) throws IllegalAccessException, ClientProtocolException, IOException {
 		send("sendEmail", em);
 	}
-	public void sendText(Text text) throws IllegalAccessException, ClientProtocolException, IOException 
-	{
+
+	public void sendText(Text text) throws IllegalAccessException, ClientProtocolException, IOException {
 		send("send", text);
 	}
-	private void send(String path, Object obj) throws IllegalAccessException, ClientProtocolException, IOException
-	{
+
+	private void send(String path, Object obj) throws IllegalAccessException, ClientProtocolException, IOException {
 		CloseableHttpClient http_client = HttpClients.createDefault();
-		HttpPost post = new HttpPost(sms_server +path);
-		
-	    post.setEntity(new StringEntity(stripTojsonField(obj)));
-	    post.setHeader("Accept", "application/json");
-	    post.setHeader("Content-type", "application/json");
-	    
-	    http_client.execute(post);
-	    http_client.close();
+		HttpPost post = new HttpPost(sms_server + path);
+
+		post.setEntity(new StringEntity(stripTojsonField(obj)));
+		post.setHeader("Accept", "application/json");
+		post.setHeader("Content-type", "application/json");
+
+		http_client.execute(post);
+		http_client.close();
 	}
 
 	private String stripTojsonField(Object obj) throws IllegalAccessException {
 		StringBuilder json = new StringBuilder();
 		json.append("{");
-		for(Field field : obj.getClass().getFields())
-	    {
+		for (Field field : obj.getClass().getFields()) {
 			Object val = field.get(obj);
-	    	if(val !=null )json.append("\""+field.getName() +"\":\"" + val+"\",");
-	    }
-		json.deleteCharAt(json.length()-1);
+			if (val != null)
+				json.append("\"" + field.getName() + "\":\"" + val + "\",");
+		}
+		json.deleteCharAt(json.length() - 1);
 		json.append("}");
-		
-		
-		return  json.toString();
+
+		return json.toString();
 	}
 
 }
