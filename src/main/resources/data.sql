f4fd5a85bc9db459f5aa7c8a1cf89f2e50d4fc59
Roice Johnson <rjohn230uncc.edu>
2020-06-04 21:08:44 -0400

    fggf

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into Member(id,email,facebook,instagram,phone,post_count,salt,salty_password,twitter,username,verified,newsletter,message_medium,notify,role)
 values(1000,'rrjohnsn2@outlook.com','kuruma.jinchuuriki','really_narutouszamki','3366181285',1,'HeA9ekya8FQ7TlQSs9sYgmNTgxfOwN+UJvht/P0Sri8=','HeA9ekya8FQ7TlQSs9sYgmNTgxfOwN+UJvht/P0Sri8=HpeaAs9sekya8FQ7TlQSs9sYgmNTgxfOwN+UJvht/P0Sri8=HeA9ekya8FQ7TlQSs9sYgmNTgxfOwN+UJvht/P0Sri8=','RoiceJohnson','ball45',TRUE,FALSE,'BOTH',TRUE,'admin');
-
+   
 insert into Contest(id,calendar,sub_count,winning_content_type,winning_content_url,winning_description,winner_id)
 values(1000,'2020-05-29 21:41:05.541',4000,'image','winnerThuMay28212623EDT2020.jpg',' this a picture of my daughter they are great this a picture of my daughter they are great this a picture of my daughter they are great this a picture of my daughter they are great ',1000);
